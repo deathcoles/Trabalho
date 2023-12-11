5a379335330b39e577a8b1cc3fa6b2622ab8e22e
PEDRO TEIXEIRA DE FREITAS <149191894+deathcoles@users.noreply.github.com>
2023-12-11 10:18:44 -0300

    exercicio.py

--- exercicio.py
+++ exercicio.py
@@ -6,9 +6,9 @@ endereco = 'https://realpython.github.io/fake-jobs/'
 resposta = requests.get(endereco)
 soup = BeautifulSoup(resposta.text, 'html.parser')
 
-for cartao in soup.find_all('div', class_='cartao'):
-    titulo = cartao.find('h2', class_='titulo').text.strip()
-    empresa = cartao.find('h3', class_='subtitulo').text.strip()
-    localizacao = cartao.find('p', class_='localizacao').text.strip()
+for cartao in soup.find_all('div', class_='card'):
+    titulo = cartao.find('h2', class_='title').text.strip()
+    empresa = cartao.find('h3', class_='subtitle').text.strip()
+    localizacao = cartao.find('p', class_='location').text.strip()
 
     print(f"Título: {titulo}\nEmpresa: {empresa}\nLocalização: {localizacao}\n{'-' * 30}")
